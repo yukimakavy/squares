6f71b132f69428231c574a1a77d79f6fc3c0f099
noam <noam@makavy.com>
2025-11-17 07:21:22 +0200

    Remove w-full from Spells and Shop to fix mobile grid width

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -3,7 +3,7 @@ import ShopItem from './ShopItem';
 
 export default function Shop() {
   return (
-    <div className="bg-gray-800 rounded-lg p-2 overflow-y-auto w-full max-h-96 md:w-64 md:max-h-none md:h-[428px]">
+    <div className="bg-gray-800 rounded-lg p-2 overflow-y-auto max-h-96 md:w-64 md:max-h-none md:h-[428px]">
       <div className="space-y-2">
         {UPGRADES.map((upgrade) => (
           <ShopItem key={upgrade.id} upgrade={upgrade} />
--- src/components/Spells.tsx
+++ src/components/Spells.tsx
@@ -3,7 +3,7 @@ import SpellButton from './SpellButton';
 
 export default function Spells() {
   return (
-    <div className="bg-gray-800 rounded-lg p-3 w-full">
+    <div className="bg-gray-800 rounded-lg p-3">
       <div className="flex gap-2 justify-between">
         {SPELLS.map((spell) => (
           <SpellButton key={spell.id} spell={spell} />
